ba21679ffc3808b05da8dbae4364cad6b8bea791
Rahul jangra <rahulljangra019@gmail.com>
2025-06-09 15:23:03 +0530

    Add files via upload

---
+++ flipkart.js
@@ -0,0 +1,9 @@
+let image1=document.getElementsByClassName("image1")
+let image2=document.getElementsByClassName("image2")
+
+for (let i = 0; i > 0; i++) {
+    if (image[i]>0.5) {
+        
+    }
+    
+}
